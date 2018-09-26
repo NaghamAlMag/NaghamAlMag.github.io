9081ba44e1c8840d1a90d135c5c115642dbe4ed9
NaghamAlMag <nagham.mag@gmail.com>
2018-09-26 15:17:46 +0200

    my-project
    My Project

---
+++ my-project/src/Controller/newEmptyPHP.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class newEmptyPHP
+{
+    public function number()
+    {
+
+        return new Response(
+            '<html><body> <input type=text value=nagham></body></html>'
+        );
+    }
+}
